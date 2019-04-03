ce25a9a1b10b4a75f7b78aa8c45f3cd7185174e6
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2019-04-03 15:13:16 +0200

    day14 part 2 with correct lru_cache buffer

--- day14.py
+++ day14.py
@@ -10,7 +10,7 @@ SALT = 'ahsbgdzn'
 TRIPLE = re.compile(r'(.)\1{2}')
 
 
-@lru_cache()
+@lru_cache(maxsize=None)
 def compute_hash(val: str) -> str:
     return md5(val.encode()).hexdigest()
 
@@ -51,3 +51,46 @@ def all_indexes(salt: str) -> int:
 
 sol = all_indexes(SALT)
 print(f'Solution for part 1: {sol[-1]}')
+
+# Part 2
+
+
+@lru_cache(maxsize=None)
+def compute_stretched_hash(val: str) -> str:
+    for _ in range(2017):
+        val = compute_hash(val)
+    return val
+
+
+def find_fivele_stretched(salt: str, index: int, char: str) -> bool:
+    """Look the next 1000 indexes for 5 times char in a hash"""
+    for i in range(index + 1, index + 1001):
+        if char * 5 in compute_stretched_hash(salt + str(i)):
+            return True
+    return False
+
+
+def next_index_stretched(salt: str) -> Generator:
+    """
+    Look for index that produce a triple, followed by a fivele whitin the next
+    1000 indexes.
+    """
+    index = 0
+    while True:
+        h = compute_stretched_hash(salt + str(index))
+        m = TRIPLE.search(h)
+        if m:
+            char = m.group(1)
+            if find_fivele_stretched(salt, index, char):
+                yield index
+        index += 1
+
+
+def all_indexes_stretched(salt: str) -> int:
+    """
+    Find the 64th valid index.
+    """
+    return list(islice(next_index_stretched(salt), 64))
+
+
+print(f'Solution for part 2: {all_indexes_stretched(SALT)[-1]}')
