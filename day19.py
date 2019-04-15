6711e1e724e760d88786e084f2e4f415b87bdcbf
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2019-04-15 12:19:22 +0200

    day19 add iter so mypy is happy. Also 5ms faster

--- day19.py
+++ day19.py
@@ -12,7 +12,7 @@ def last_elf(num_elves: int) -> int:
     # round has taken the presents, or 0 if not (it will take the presents of
     # the first elf, and he should be removed.)
     start_elf = 1
-    next_slice = range(1, num_elves + 1)
+    next_slice = iter(range(1, num_elves + 1))
     while num_elves > 1:
         next_slice = compress(next_slice, cycle([start_elf, 1 - start_elf]))
         num_elves, start_elf = (num_elves + start_elf) // 2, (num_elves + start_elf) % 2
@@ -20,3 +20,4 @@ def last_elf(num_elves: int) -> int:
 
 
 print(f"Solution for part 1: {last_elf(INPUT)}")
+
